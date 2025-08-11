617e06679707fb07aa5f0e9ddc8d3ed60769e60f
Jonathan Nilsen <jonathan.nilsen@nordicsemi.no>
2025-08-11 10:14:42 +0200

    modules: support external sysbuild cmake/Kconfig files in modules/
    Extend modules.cmake to also support placing module sysbuild
    cmake in modules/<module name>/sysbuild/CMakeLists.txt,
    and module sysbuild Kconfig in module/<module_name>/Kconfig.sysbuild.
    
    Signed-off-by: Jonathan Nilsen <jonathan.nilsen@nordicsemi.no>

--- modules/modules.cmake
+++ modules/modules.cmake
@@ -19,3 +19,24 @@ foreach(module ${kconfig_modules})
 
   set(ZEPHYR_${MODULE_NAME_UPPER}_KCONFIG ${module_dir}/Kconfig)
 endforeach()
+
+file(GLOB sysbuild_cmake_modules "${CMAKE_CURRENT_LIST_DIR}/*/sysbuild/CMakeLists.txt")
+
+foreach(module ${sysbuild_cmake_modules})
+  cmake_path(GET module PARENT_PATH module_sysbuild_dir)
+  cmake_path(GET module_sysbuild_dir PARENT_PATH module_dir)
+  cmake_path(GET module_dir STEM module_name)
+  zephyr_string(SANITIZE TOUPPER MODULE_NAME_UPPER ${module_name})
+
+  set(SYSBUILD_${MODULE_NAME_UPPER}_CMAKE_DIR ${module_sysbuild_dir})
+endforeach()
+
+file(GLOB sysbuild_kconfig_modules "${CMAKE_CURRENT_LIST_DIR}/*/Kconfig.sysbuild")
+
+foreach(module ${sysbuild_kconfig_modules})
+  cmake_path(GET module PARENT_PATH module_dir)
+  cmake_path(GET module_dir STEM module_name)
+  zephyr_string(SANITIZE TOUPPER MODULE_NAME_UPPER ${module_name})
+
+  set(SYSBUILD_${MODULE_NAME_UPPER}_KCONFIG ${module_dir}/Kconfig.sysbuild)
+endforeach()
